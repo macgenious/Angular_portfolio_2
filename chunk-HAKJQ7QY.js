afed81353e08b2bd34519d9b8b6655337a2e8d45
Alejandroouu <vasilealejandro10@gmail.com>
2025-11-22 19:06:53 +0100

    Deploy Angular portfolio to GitHub Pages

---
+++ chunk-HAKJQ7QY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YVHDTM5E.js";import"./chunk-HZ2MWCNJ.js";import"./chunk-4EMDSEIC.js";export{a as ProjectsComponent};
---
+++ chunk-IJ5JJBG7.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-LXUXCX4M.js";import"./chunk-4EMDSEIC.js";export{a as SkillsComponent};
